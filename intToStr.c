d87c72b61556e7e1ad0ea1ffa518c6bbb7fe5801
nadazkai21 <nadazaki019@gmail.com>
2023-11-11 20:22:18 +0200

    priting the right number of chareceters

--- intToStr.c
+++ intToStr.c
@@ -1,48 +1,58 @@
 #include "main.h"
+
 /**
- * intToStr - int to char
- * @x : integer to be convereted
- * @flag: fla gif the number is negative or not
- * @negativeSignPrinted:: fag if negative sign is printed or not
- * Return: returns the number of the charecters printed including
- * the negative sign if the sign is neagtive
-*/
+ * intToStr - Convert integer to char
+ * @x: Integer to be converted
+ * @flag: Flag if the number is negative or not
+ * @negativeSignPrinted: Flag if the negative sign is printed or not
+ *
+ * Return: Returns the number of characters printed, including the negative sign if the number is negative
+ */
 int intToStr(int x, int flag, int negativeSignPrinted)
 {
-	int converted = 0, rem, checker, counter = 0;
-	char c;
+    int converted = 0, rem, checker;
+    static int counter = 0;
+    char c;
 
-	if (x < 0)
-	{
-		flag = 1;
-		checker = (x * (-1));
-	}
-	else if (x == 0)
-	{
-		writechar('0');
-		return (1);
-	}
-	else
-	{
-		flag = 0;
-		checker = x;
-	}
+    if (x < 0)
+    {
+        flag = 1;
+        checker = -x;
+    }
+    else if (x == 0)
+    {
+        writechar('0');
+        return 1;
+    }
+    else
+    {
+        flag = 0;
+        checker = x;
+    }
 
-	if (checker > 0)
-	{
-		rem = checker % 10;
-		converted = rem + 48;
-		c = converted;
-		checker = checker / 10;
-		if (flag == 1 && !negativeSignPrinted)
-		{
-			writechar('-');
-			negativeSignPrinted = 1;
-			counter++;
-		}
-		intToStr(checker, flag, negativeSignPrinted);
-		counter = counter + (writechar(c));
-	}
+    if (flag == 1 && !negativeSignPrinted)
+    {
+        writechar('-');
+        negativeSignPrinted = 1;
+        counter++;
+    }
 
-	return (counter);
+    
+    while (checker > 0)
+    {
+        rem = checker % 10;
+        converted = converted * 10 + rem;
+        checker = checker / 10;
+    }
+
+    
+    while (converted > 0)
+    {
+        rem = converted % 10;
+        c = rem + '0';
+        counter += writechar(c);
+        converted = converted / 10;
+    }
+
+    return counter;
 }
