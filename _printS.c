57902a799e25d91e406bbc66e0e2c71c37f6a030
AbduHany <abdu.hany@gmail.com>
2023-11-12 21:19:39 +0200

    Modify _printS.c

--- _printS.c
+++ _printS.c
@@ -14,38 +14,37 @@ int _printS(va_list args)
 	int len = 0, i, printed = 0, tmp;
 
 	str = va_arg(args, char *);
+	if (str == NULL)
+		str = "(null)";
 	for (i = 0; str[i]; i++)
 		len++;
 	for (i = 0; i < len; i++)
 	{
 		int y = str[i];
+
 		if (y >= 32 && y <= 127)
 			printed += writechar(str[i]);
 		else
 		{
-			char hex[3] = {'0', '0', '\0'};
-			int x = str[i], j = 1;
+			char hex[5] = {'\\', 'x', '0', '0', '\0'};
+			int x = str[i], j = 3;
 
-			printed += writestr("\\x", 2);
-			while (x != 0 && j >= 0)
+			while (x != 0 && j >= 2)
 			{
 				tmp = x % 16;
 				if (tmp < 10)
 				{
 					tmp += 48;
-					hex[j] = tmp;
-					j--;
+					hex[j--] = tmp;
 				}
 				else
 				{
 					tmp += 55;
-					hex[j] = tmp;
-					j--;
+					hex[j--] = tmp;
 				}
 				x /= 16;
-
 			}
-			printed += writestr(hex, 2);
+			printed += writestr(hex, 4);
 		}
 	}
 	return (printed);
