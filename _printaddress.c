ed79040753c02a538f8a21b30d7c690c7e89026a
nadazkai21 <nadazaki019@gmail.com>
2023-11-13 20:00:23 +0200

    committing bbefore pulling

--- _printaddress.c
+++ _printaddress.c
@@ -1,16 +1,15 @@
 #include "main.h"
 /**
- * _printaddress - printing the address in a 12 
+ * _printaddress - printing the address in a 12
  * digit hexa decimal syntax
  * @args: the argument it would work on
  * Return: the number of charecters printed
 */
 int _printaddress(va_list args)
 {
-    unsigned long int  num, temp;
+	unsigned long int  num, temp;
 	int len = 0, rem, counter = 0, i = 0, j;
-	char *ptr;
-    char *prefix = "0x";
+	char *ptr, *prefix = "0x";
 
 	num = va_arg(args, unsigned long int);
 	if (num == 0)
@@ -41,14 +40,13 @@ int _printaddress(va_list args)
 		i++;
 		num = num / 16;
 	}
-    j = 1;
-    while (j >= 0)
-    {
-        ptr[i] = prefix[j];
-        j--;
-        i++;
-    }
-    
+	j = 1;
+	while (j >= 0)
+	{
+		ptr[i] = prefix[j];
+		j--;
+		i++;
+	}
 	for (i = (len + 2 - 1); i >= 0; i--) /*printing the string in ptr*/
 	{
 		counter = counter + writechar(ptr[i]);
