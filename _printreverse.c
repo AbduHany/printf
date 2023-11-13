f749153d22d38f2c40361f8efd3736de5f272cd6
nadazkai21 <nadazaki019@gmail.com>
2023-11-13 21:05:53 +0200

    13, fixig betty

--- _printreverse.c
+++ _printreverse.c
@@ -6,28 +6,28 @@
 */
 int _printreverse(va_list args)
 {
-    char *ptr;
-    int i = 0, len = 0, counter = 0;
+	char *ptr;
+	int i = 0, len = 0, counter = 0;
 
-    ptr = va_arg(args, char*);
-    if (ptr == NULL)
-    {
-        return (-1);
-    }
-    if (*ptr == '\0')
-    {
-        return 0; /* Empty string, nothing to reverse */
-    }
+	ptr = va_arg(args, char*);
+	if (ptr == NULL)
+	{
+		return (-1);
+	}
+	if (*ptr == '\0')
+	{
+		return (0); /* Empty string, nothing to reverse */
+	}
 
-    while (ptr[i] != '\0')
-    {
-        len++;
-        i++;
-    }
+	while (ptr[i] != '\0')
+	{
+		len++;
+		i++;
+	}
 
-    for (i = (len - 1); i >= 0; i--)
-    {
-        counter = counter + writechar(ptr[i]);   
-    }
-    return(counter);
+	for (i = (len - 1); i >= 0; i--)
+	{
+		counter = counter + writechar(ptr[i]);
+	}
+	return (counter);
 }
